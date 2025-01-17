57837dbf68f3fed4e85dcef7f30794d0d4c1f502
darshan081289 <87810752+darshan081289@users.noreply.github.com>
2025-01-17 19:30:37 +0530

    Create index.js

---
+++ tradeindia_integration/public/js/index.js
@@ -0,0 +1,11 @@
+// tradeindia_integration/tradeindia_integration/public/js/index.js
+frappe.provide('tradeindia_integration');
+
+tradeindia_integration.setup = function() {
+    // Setup code here
+};
+
+// Initialize when ready
+$(document).ready(function() {
+    tradeindia_integration.setup();
+});
