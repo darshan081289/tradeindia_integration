61a67263635ca41b5ea7807ba156cee3904eedf4
darshan081289 <87810752+darshan081289@users.noreply.github.com>
2025-01-17 19:20:25 +0530

    Update tradeindia_integration.js

--- tradeindia_integration/public/js/tradeindia_integration.js
+++ tradeindia_integration/public/js/tradeindia_integration.js
@@ -1,1 +1,14 @@
-
+{
+    "name": "tradeindia_integration",
+    "version": "1.0.0",
+    "description": "TradeIndia Integration for ERPNext",
+    "main": "index.js",
+    "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "keywords": [
+        "frappe"
+    ],
+    "author": "Your Name",
+    "license": "MIT"
+}
